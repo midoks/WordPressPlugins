8cec6cdbaa10ecfce65e2b6a4fff0f9f5468031d
midoks <midoks@163.com>
2014-01-06 21:00:47 +0800

    修复一个BUG

---
+++ wp-cache-html/lib/html.class.php
@@ -0,0 +1,25 @@
+<?php
+class html{
+
+	
+	public function __construct(){
+	
+	}
+
+	public function write($fn, $content){
+	}
+
+	public function read($fn){
+	}
+
+	public function ftime($fn){
+	}
+
+	public function fexists($fn){
+	}
+
+	public function getUrl($fn){
+	}
+
+}
+?>
