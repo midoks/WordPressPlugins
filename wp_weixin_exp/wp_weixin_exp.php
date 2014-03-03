c2ff2f61f0de0e994d123ef2652229018c915159
midoks <midoks@163.com>
2014-03-03 23:45:08 +0800

    wiwexinweixni

---
+++ wp_weixin_exp/wp_weixin_exp.php
@@ -0,0 +1,96 @@
+<?php
+/*
+Plugin Name: WP微信机器人测试
+Plugin URI: http://midoks.cachecha.com/
+Description: Weixin connected to the WordPress, use the information you faster (微信连接Wordpress,使你的传播的信息更快)
+Version: 0.0.1
+Author: Midoks
+Author URI: http://midoks.cachecha.com/
+*/
+
+//define your token
+define("TOKEN", "midoks");
+
+
+add_action('init', 'iweixin_robot_start', 0);
+
+
+function iweixin_robot_start(){
+	if(isset($_GET['imidoks']) ){//sign
+		$wechatObj = new midokswechatCallbackapiTest();
+		$wechatObj->valid();
+	}
+}
+
+
+class midokswechatCallbackapiTest
+{
+	public function valid()
+    {
+        $echoStr = $_GET["echostr"];
+        //valid signature , option
+        if($this->checkSignature()){
+			echo $echoStr;
+			//echo 'ggggggggg';
+        	exit;
+        }
+    }
+
+    public function responseMsg()
+    {
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+      	//extract post data
+		if (!empty($postStr)){
+                
+              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+                $fromUsername = $postObj->FromUserName;
+                $toUsername = $postObj->ToUserName;
+                $keyword = trim($postObj->Content);
+                $time = time();
+                $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";             
+				if(!empty( $keyword ))
+                {
+              		$msgType = "text";
+                	$contentStr = "Welcome to wechat world!";
+                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                	echo $resultStr;
+                }else{
+                	echo "Input something...";
+                }
+
+        }else {
+        	echo "";
+        	exit;
+        }
+    }
+		
+	private function checkSignature()
+	{
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];	
+        		
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		sort($tmpArr);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
+		
+		if( $tmpStr == $signature ){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
+
+?>
